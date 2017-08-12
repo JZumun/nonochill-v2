21c9fc00b93793654b79c621f3350655466694e8
John Reinier Bulaong <rein.bulaong@yahoo.com>
2017-08-13 03:25:54 +0800

    Fix highlight bug for ids with null props

--- src/mixins/highlighter.js
+++ src/mixins/highlighter.js
@@ -1,6 +1,7 @@
 import eventBus from "../pubsub/Bus"
 import { TILE_HIGHLIGHT_EVT } from "../pubsub/Events"
 
+const equalAndValued = (a,b) => a != null && a === b;
 export default {
 	props: {
 		id: Object
@@ -15,7 +16,8 @@ export default {
 	},
 	created() {
 		eventBus.$on(TILE_HIGHLIGHT_EVT,high=>{
-			this.highlight = (high.x == this.id.x) || (high.y == this.id.y)
+			this.highlight = equalAndValued(high.x, this.id.x)
+												|| equalAndValued(high.y, this.id.y)
 		})
 	}
 }
