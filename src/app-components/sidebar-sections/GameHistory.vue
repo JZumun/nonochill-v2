09c02cc3163beccff2a3444d4da4f1bcc62e60cf
John Reinier Bulaong <rein.bulaong@yahoo.com>
2017-08-14 22:00:27 +0800

    Undo/Redo Capability!

---
+++ src/app-components/sidebar-sections/GameHistory.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+	fieldset
+		legend History
+		div.buttons
+			button(:disabled="past.length == 0" @click="undo") Undo
+			button(:disabled="future.length == 0" @click="redo") Redo
+</template>
+
+<script>
+	import debounce from "debounce";
+	import Bus from "../../pubsub/Bus"
+	import { TILE_TOGGLE_EVT, GAME_START_EVT } from "../../pubsub/Events"
+
+	export default {
+		data() {
+			return {
+				past: [],
+				future: [],
+				staged: []
+			}
+		},
+		methods: {
+			commitMoves: debounce(function(){
+				this.past.push(this.staged);
+				this.staged = [];
+			},500),
+			queueMove: function(move) {
+				if (move.undone) return;
+
+				this.future = []
+				this.staged.push(move);
+				this.commitMoves();
+			},
+			undo() {
+				const moves = this.past.pop();
+				moves.reverse().forEach((move)=>{
+					Bus.$emit(TILE_TOGGLE_EVT,{
+						undone: true,
+						tile: move.tile,
+						next: move.curr
+					})
+				})
+				this.future.push(moves);
+			},
+			redo() {
+				const moves = this.future.pop();
+				moves.reverse().forEach((move)=>{
+					Bus.$emit(TILE_TOGGLE_EVT,{
+						undone: true,
+						tile: move.tile,
+						next: move.next
+					})
+				})
+				this.past.push(moves);
+			},
+			clear() {
+				this.future = [];
+				this.past = [];
+				this.staged = [];
+			}
+		},
+		created() {
+			Bus.$on(TILE_TOGGLE_EVT,this.queueMove);
+			Bus.$on(GAME_START_EVT,this.clear);
+		}
+	}
+</script>
+
+<style scoped>
+	.buttons {
+		display:grid;
+		grid-gap:0.5em;
+		grid-auto-flow: column;
+	}
+	button {
+		margin-top:0;
+		display:inline-block;
+	}
+</style>
