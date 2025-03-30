dde323e63c9e5edc2909f0203fa1052a05bd222c
John Reinier Bulaong <rein.bulaong@yahoo.com>
2025-03-30 16:04:08 +0800

    refactor footer

--- src/components/app/sidebar-sections/Footer.vue
+++ src/components/app/sidebar-sections/Footer.vue
@@ -2,19 +2,18 @@
     footer
         span
             | Created by&nbsp;
-            a.author(href="https://github.com/JZumun")
-                img(src="https://avatars3.githubusercontent.com/u/10479613")
+            a.author(href="https://jzumun.ph")
+                img(src="https://www.jzumun.ph/images/profile.png")
                 | JZumun
-            | &nbsp;using #[a(href="https://vuejs.org/") Vue JS].&nbsp;
-        
+            | &nbsp;using #[a(href="https://vuejs.org/") Vue JS].
+        span Sound effects from #[a(href="https://pixabay.com/sound-effects/") Pixabay].
+        span Music by #[a(href="https://cchound.com/post/173227390201/source-bandcamp-author-aerocity-cc-by-can") Aerocity] under #[a(href="https://creativecommons.org/licenses/by/4.0/") CCBY].
         span(v-if="bgAuthor") 
-            | Background image by&nbsp; 
-            a(:href="actualBgAuthorLink" target="_blank") {{bgAuthor}} 
-            | &nbsp;from&nbsp; 
+            | Background image by  
+            a(:href="actualBgAuthorLink" target="_blank") {{bgAuthor}}
+            | &nbsp;on  
             a(href="https://unsplash.com?utm_source=nonochill&utm_medium=referral") Unsplash.
-            | &nbsp;
-        span Music by #[a(href="https://cchound.com/post/173227390201/source-bandcamp-author-aerocity-cc-by-can") Aerocity] under #[a(href="https://creativecommons.org/licenses/by/4.0/") CCBY].&nbsp;
-        span Hosted on #[a(href="https://github.com/JZumun/nonochill-v2") Github].&nbsp;
+        span Hosted on #[a(href="https://github.com/JZumun/nonochill-v2") Github].
 </template>
 <script>
 import { mapState } from "vuex";
@@ -36,17 +35,23 @@ export default {
         opacity 0.5
         padding 2em
         margin-top auto
-        text-align center
+        text-align left
 
     span
         display inline-block
-        white-space nowrap
+        line-height 2
+
+        &::after
+            content " "
+            width: 1ch
+            display inline-block
 
     img
         width 2em
         vertical-align middle
         border-radius 50%
         margin-right 0.5ch
+        // margin-bottom: -1em
 
     .author
         white-space nowrap
